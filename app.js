0f37c4f504d35591a30186e3a40226d29787e6d3
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-11-13 15:21:51 -0800

    landscaper: user can use a team to cut grass

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 // Collection of tools available to the player. 
 // A tool object includes the tool name, money it generates and price to buy the tool.
 const tools = [
-    {name: 'teeth', generate:5, price:0},
-    {name: 'rusty scissor', generate:5, price:5},
-    {name: 'lawnmower', generate:50, price:25 },
-    {name: 'battery-powered lawnmower', generate:100, price:250},
-    {name: 'team', price:500}
+    {name:'teeth', generate:5, price:0},
+    {name:'rusty scissor', generate:5, price:5},
+    {name:'push lawnmower', generate:50, price:25 },
+    {name:'battery-powered lawnmower', generate:100, price:250},
+    {name:'team', generate:250, price:500}
 ]
