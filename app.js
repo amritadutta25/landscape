8273dac9687b889564e3fac449c0055302ef0309
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-11-13 15:29:33 -0800

    landscaper: win scenario

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ const tools = [
     {name:'battery-powered lawnmower', generate:100, price:250},
     {name:'team', generate:250, price:500}
 ]
+
+// player object keeps track of the tool he is using, amount of money he has and if he won the game or not
+const player = {
+    tool: 0, // index of the tools array, player starts with the first basic tool
+    money: 0, // initially the player has $0
+    winStatus: False // at the start of the game player has not won the game
+}
+
+function winCondition()
+{  // win the game when you have a team of starving students and $1000
+    if ((player.tool === 'team') && (player.money >= 1000))
+    {
+        // send a message to the user telling them, they've won
+        alert("Congrats! You have won!")
+    }
+}
+
