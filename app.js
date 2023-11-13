69a916f4220a18db46d0265882cf95878506860e
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-11-13 14:37:53 -0800

    landscaper: user can use teeth to cut grass

---
+++ app.js
@@ -0,0 +1,7 @@
+//Landscaping business
+
+// collection of tolls available to the player. 
+// Tool object includes the tool name, money it generates and price to buy the tool.
+const tools = [
+    {name: 'teeth', generate:5, price:0}
+]
